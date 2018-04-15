f5279ad7686d26107c923ea3c0e37f6d0e78c40a
Abhishek Suresh <chuninpeter@yahoo.com>
2018-04-15 20:44:35 +0530

    server code

--- Nathaniel/servers/test_client.py
+++ Nathaniel/servers/test_client.py
@@ -1,10 +1,16 @@
 import socket               # Import socket module
-
+import sys
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 1234                # Reserve a port for your service.
-
 s.connect((host, port))
-s.send('Who are you'.encode('ascii'))
-print (s.recv(1024))
+while True:
+    inp=input("User:")
+    s.send(inp.encode('ascii'))
+    recv=s.recv(1024)
+    if recv=='stop':
+        s.close
+        sys.exit()
+        break
+    print ("Nathaniel:",recv.decode('ascii'))
 s.close                     # Close the socket when done
